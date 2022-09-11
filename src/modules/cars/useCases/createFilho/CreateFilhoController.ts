49a8928d60ec76341186290d98f9fcfc80b464aa
Daniel Waite <danielwaite6@hotmail.com>
2022-09-10 23:02:08 -0300

    first commit

--- src/modules/cars/useCases/createFilho/CreateFilhoController.ts
+++ src/modules/cars/useCases/createFilho/CreateFilhoController.ts
@@ -1,11 +1,13 @@
 import { Request, Response } from 'express'
 import { container } from 'tsyringe'
-import { CreateFilhoUseCase } from './CreateFilhoUseCase'
+import { CreateFilhoUseCase } from './CreateFilhoUseCase';
 
 class CreateFilhoController {
 
     async handle(req: Request, res: Response): Promise<Response> {
         const { name, mae_id } = req.body
+
+
         const createFilhoUseCase = container.resolve(CreateFilhoUseCase)
         await createFilhoUseCase.execute({ mae_id, name })
         return res.status(201).send()
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,15 @@
 import "reflect-metadata"
 import express from 'express';
-import cors from 'cors';
+//import cors from 'cors';
 import "./database"
 import "./shared/container"
 import { router } from './routes'
 
 
 const app = express();
-app.use(cors({ origin: true, credentials: true }));
 app.use(express.json());
+
+//app.use(cors({ origin: true, credentials: true }));
 
 app.use(router)
 
