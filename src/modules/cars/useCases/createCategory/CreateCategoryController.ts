cffde2990cdb0302480a46d11d2482e85649273e
Daniel Waite <danielwaite6@hotmail.com>
2022-09-14 23:55:44 -0300

    first commit

--- src/modules/cars/useCases/createCategory/CreateCategoryController.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryController.ts
@@ -12,7 +12,7 @@ class CreateCategoryController {
 
         await createCategoryUseCase.execute({ name, description })
 
-        return res.status(201).send().json({ name, description })
+        return res.json({ name, description })
     }
 
 }
