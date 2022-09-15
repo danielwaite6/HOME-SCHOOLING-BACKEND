a05f969c9e869b5a11b4d459a52695ba03f0db8b
Daniel Waite <danielwaite6@hotmail.com>
2022-09-14 22:41:40 -0300

    first commit

--- src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
@@ -16,13 +16,19 @@ class CreateCategoryUseCase {
 
     async execute({ name, description }: IRequest): Promise<void> {
 
-        const categoryAlreadyExists = await this.categoriesRepository.findByName(name)
 
-        if (categoryAlreadyExists) {
-            throw new Error("Category Already Exists.")
+        try {
+            const categoryAlreadyExists = await this.categoriesRepository.findByName(name)
+
+            if (categoryAlreadyExists) {
+                throw new Error("Category Already Exists.")
+            }
+            await this.categoriesRepository.create({ name, description })
+        } catch (error) {
+            throw new Error(error)
         }
 
-        await this.categoriesRepository.create({ name, description })
+
     }
 }
 export { CreateCategoryUseCase }
