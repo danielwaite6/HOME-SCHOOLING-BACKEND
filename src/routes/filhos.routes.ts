d9e13c34c0979a80aa7bbeb1cb53496801c16db6
Daniel Waite <danielwaite6@hotmail.com>
2022-09-29 00:15:13 -0300

    First Commit

--- src/routes/filhos.routes.ts
+++ src/routes/filhos.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express'
 import { CreateFilhoController } from '../modules/cars/useCases/createFilho/CreateFilhoController'
+import { ListFilhosController } from '../modules/cars/useCases/listFilhos/ListFilhosController'
 
 
 
@@ -7,8 +8,10 @@ const filhosRoutes = Router()
 // Repository --> Camada de acesso a dados.
 
 const createFilhoController = new CreateFilhoController()
+const listFilhosController = new ListFilhosController()
 
 filhosRoutes.post('/', createFilhoController.handle)
+filhosRoutes.get('/', listFilhosController.handle)
 
 
 
