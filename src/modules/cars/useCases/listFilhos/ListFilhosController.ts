fa0dd12aa020a26c28fb7aa72d8a4839e8db1c76
Daniel Waite <danielwaite6@hotmail.com>
2022-10-06 21:52:46 -0300

    First Commit

--- src/modules/cars/useCases/listFilhos/ListFilhosController.ts
+++ src/modules/cars/useCases/listFilhos/ListFilhosController.ts
@@ -7,8 +7,8 @@ class ListFilhosController {
 
     async handle(req: Request, res: Response): Promise<Response> {
 
-        const { mae_id } = req.params
-        console.log('req.params: ', mae_id);
+        const { mae_id } = req.query
+        console.log('req.query: ', mae_id);
 
 
         const listFilhosUseCase = container.resolve(ListFilhosUseCase)
