c9818bf8b2c50025fa6a1822d1dadab5364295c3
Daniel Waite <danielwaite6@hotmail.com>
2022-10-07 23:19:56 -0300

    First Commit

--- src/modules/cars/useCases/listActivitys/ListActivitysController.ts
+++ src/modules/cars/useCases/listActivitys/ListActivitysController.ts
@@ -12,10 +12,12 @@ class ListActivitysController {
 
 
 
-        /*const listActivitysUseCase = container.resolve(ListActivitysUseCase)
+        const listActivitysUseCase = container.resolve(ListActivitysUseCase)
+
         const activitys = await listActivitysUseCase.execute(mae_id, filho_id)
-        return res.status(201).json(activitys)*/
-        return res.status(201).json({})
+
+        return res.status(201).json(activitys)
+        //return res.status(201).json({})
     }
 }
 export { ListActivitysController }
